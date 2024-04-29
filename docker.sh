a3090d7a7576909ce65d924dd7904a52d5c1d0a2
wangyang <1749748955@qq.com>
2024-04-29 21:51:47 +0800

    update

--- docker.sh
+++ docker.sh
@@ -2,3 +2,5 @@
 
 docker build -t wybioinfo/nextflow:23.11.0  .
 docker tag  wybioinfo/nextflow:23.11.0  registry.cn-hangzhou.aliyuncs.com/wybioinfo/nextflowdev:23.11.0
+
+docker push registry.cn-hangzhou.aliyuncs.com/wybioinfo/nextflowdev:23.11.0
